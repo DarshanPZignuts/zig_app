43115f73b6f02488c8d03c4933652ee87b3117b4
DarshanVanol <darshanvano009@gmail.com>
2022-12-31 10:45:03 +0530

    added bottomnavigationbar in dashboard page,drawer

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -9,10 +9,65 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+
+ int currentIndex=2;
+
+ static const List<Widget> _widgetslist =[
+  Center(child: Text("This is Categories Page"),),
+  Center(child: Text("This is Vendors Page"),),
+  Center(child: Text("This is Home Page"),),
+  Center(child: Text("This is  List Page"),),
+  Center(child: Text("This is More Page"),),
+ ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Project ZIG"),centerTitle: true, backgroundColor: Colors.amber,),
+      drawer: Drawer(child: ListView(
+
+        children: [
+          UserAccountsDrawerHeader(
+            decoration: BoxDecoration(color: Colors.amber),
+            accountEmail: Text("darshanvano009@gmail.com"),
+            accountName: Text("Darshan Vanol"),
+            ),
+            ListTile(title: Text("Payments"),trailing: Icon(Icons.payment),style: ListTileStyle.drawer,),
+            Divider(color: Colors.grey.shade500),
+            ListTile(title: Text("Change Password"),trailing: Icon(Icons.password),style: ListTileStyle.drawer,),
+              Divider(color: Colors.grey.shade500),
+            ListTile(title: Text("Address"),trailing: Icon(Icons.house_siding_sharp),style: ListTileStyle.drawer,),
+              Divider(color: Colors.grey.shade500),
+            ListTile(title: Text("Other"),trailing: Icon(Icons.arrow_forward),style: ListTileStyle.drawer,),
+              Divider(color: Colors.grey.shade500),
+
+
+          
+
+        ],
+      )),
+
+
+      body: _widgetslist[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index){
+          setState(() {
+            currentIndex=index;
+          });
+        },
+        fixedColor: Colors.amber,
+        iconSize: 30,
+        
+
+        type: BottomNavigationBarType.fixed,
+        items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.category),label: "Categories"),
+        BottomNavigationBarItem(icon: Icon(Icons.face),label: "Vendor"),
+        BottomNavigationBarItem(icon: Icon(Icons.home,size: 45),label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.list),label: "List"),
+        BottomNavigationBarItem(icon: Icon(Icons.more),label: "More"),
+      ]),
+      appBar: AppBar(title: const Text("Dashboard"),centerTitle: true, backgroundColor: Colors.amber,),
     );
   }
 }
