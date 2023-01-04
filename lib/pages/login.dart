268273759d6b76be8584b385ae01a39463940eb7
Zignuts Technolab <darshanvano009@gmail.com>
2023-01-04 19:30:37 +0530

    changes in login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:zig_project/authentication/auth.dart';
 import 'package:zig_project/pages/dashboard.dart';
+import 'package:zig_project/pages/resetpassword.dart';
 import 'package:zig_project/pages/signin.dart';
 
 class LogIn extends StatefulWidget {
@@ -27,7 +28,7 @@ class _LogInState extends State<LogIn> {
     required String? validate(String? s),
   }) {
     return Container(
-        height: 60,
+        height: 80,
         width: 300,
         child: Padding(
             padding: const EdgeInsets.only(left: 8, right: 8),
@@ -35,14 +36,14 @@ class _LogInState extends State<LogIn> {
               validator: validate,
               cursorColor: Colors.amber,
               cursorHeight: 20,
-              style: TextStyle(color: Colors.grey.shade600),
+              style: TextStyle(color: Colors.amber),
               controller: controller,
               obscureText: obscureText,
               decoration: InputDecoration(
-                border: const OutlineInputBorder(
+                border: const UnderlineInputBorder(
                     borderSide: BorderSide(color: Colors.black),
                     borderRadius: BorderRadius.all(Radius.circular(20))),
-                focusedBorder: const OutlineInputBorder(
+                focusedBorder: const UnderlineInputBorder(
                     borderSide: BorderSide(color: Colors.amber),
                     borderRadius: BorderRadius.all(Radius.circular(20))),
                 suffixIcon: isPassword
@@ -75,216 +76,151 @@ class _LogInState extends State<LogIn> {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.amber,
-      body: SafeArea(
+      body: Form(
+        key: _formkey,
         child: Container(
-            child: Stack(children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              "Login",
+              style: TextStyle(color: Colors.amber, fontSize: 30),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Welcome back,",
+              style: TextStyle(color: Colors.grey.shade500),
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            _buildInput(
+                label: "Email",
+                obscureText: false,
+                isPassword: false,
+                validate: (String? val) {},
+                controller: _emailcontroller),
+            const SizedBox(
+              height: 30,
+            ),
+            _buildInput(
+                isPassword: true,
+                label: "Password",
+                obscureText: !showPassword,
+                validate: (String? val) {
+                  if (val!.isEmpty || val == null) {
+                    return "* required";
+                  } else if (val.length < 6) {
+                    return "Invalid password";
+                  } else if (!RegExp(r"[a-zA-Z]").hasMatch(val)) {
+                    return "Invalid password";
+                  } else if (!RegExp(r"[0-9]").hasMatch(val)) {
+                    return "Invalid password";
+                  }
+                },
+                controller: _passwordcontroller),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Container(
-                  color: Colors.amber,
-                  height: height * 0.3,
-                  width: width,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 30, top: 65),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          "Login",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w400,
-                              fontSize: 27),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          "Welcome back",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
+                Text(
+                  "Forget Password?",
+                  style: TextStyle(color: Colors.grey.shade600),
                 ),
-                Container(
-                  color: Colors.white,
-                  height: height * 0.7,
-                  width: width,
-                  child: Padding(
-                      padding: const EdgeInsets.only(top: 480),
-                      child: Column(
-                        children: [
-                          const Icon(
-                            Icons.android,
-                            color: Colors.amber,
-                            size: 55,
-                          ),
-                          Text(
-                            "©2022 Darshankumar vanol",
-                            style: TextStyle(color: Colors.grey.shade400),
-                          )
-                        ],
+                Padding(
+                  padding: const EdgeInsets.only(right: 55),
+                  child: TextButton(
+                      onPressed: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ResetPassword()));
+                        // try {
+                        //   await FirebaseAuth.instance
+                        //       .sendPasswordResetEmail(
+                        //           email: "darshanvano009@gmail.com")
+                        //       .whenComplete(() =>
+                        //           ScaffoldMessenger.of(context)
+                        //               .showSnackBar(SnackBar(
+                        //                   content: Text("Success!!"))));
+                        // } catch (e) {
+                        //   ScaffoldMessenger.of(context).showSnackBar(
+                        //       SnackBar(content: Text(e.toString())));
+                        // }
+                      },
+                      child: const Text(
+                        "RESET",
+                        style: TextStyle(color: Colors.amber),
                       )),
+                ),
+                const SizedBox(
+                  width: 15,
                 )
               ],
             ),
-          ),
-          Center(
-            child: Form(
-              key: _formkey,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 130),
-                child: Container(
-                  height: 335,
-                  width: 340,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    boxShadow: [
-                      BoxShadow(
-                          offset: const Offset(2, 2),
-                          color: Colors.grey.shade400,
-                          blurRadius: 10,
-                          spreadRadius: 1)
-                    ],
-                  ),
-                  child: Column(children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    _buildInput(
-                        label: "Email",
-                        obscureText: false,
-                        isPassword: false,
-                        validate: (String? val) {},
-                        controller: _emailcontroller),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    _buildInput(
-                        isPassword: true,
-                        label: "Password",
-                        obscureText: !showPassword,
-                        validate: (String? val) {
-                          if (val!.isEmpty || val == null) {
-                            return "* required";
-                          } else if (val.length < 6) {
-                            return "Invalid password";
-                          } else if (!RegExp(r"[a-zA-Z]").hasMatch(val)) {
-                            return "Invalid password";
-                          } else if (!RegExp(r"[0-9]").hasMatch(val)) {
-                            return "Invalid password";
-                          }
-                        },
-                        controller: _passwordcontroller),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Forget Password?",
-                          style: TextStyle(color: Colors.grey.shade600),
-                        ),
-                        TextButton(
-                            onPressed: () async {
-                              try {
-                                await FirebaseAuth.instance
-                                    .sendPasswordResetEmail(
-                                        email: "darshanvano009@gmail.com")
-                                    .whenComplete(() =>
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(SnackBar(
-                                                content: Text("Success!!"))));
-                              } catch (e) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(content: Text(e.toString())));
-                              }
-                            },
-                            child: const Text(
-                              "RESET",
-                              style: TextStyle(color: Colors.amber),
-                            )),
-                        const SizedBox(
-                          width: 15,
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (_formkey.currentState!.validate()) {
-                          setState(() {
-                            isloading = true;
-                          });
-                          final user = await _auth.logInwithEmailandpassword(
-                              _emailcontroller.text, _passwordcontroller.text);
-                          if (user != null) {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: ((context) => Dashboard())));
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(content: Text("Invalid Credentials")));
-                            setState(() {
-                              isloading = false;
-                            });
-                          }
-                        }
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.amber),
-                        fixedSize: MaterialStatePropertyAll(
-                            Size(width * 0.6, height * 0.02)),
-                      ),
-                      child: isloading
-                          ? Container(
-                              height: 10,
-                              child: CircularProgressIndicator(
-                                color: Colors.white,
-                              ),
-                            )
-                          : const Text(
-                              "LOGIN",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w400),
-                            ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Don't have account?",
-                          style: TextStyle(color: Colors.grey.shade500),
-                        ),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: ((context) => SignIn())));
-                            },
-                            child: const Text(
-                              "Create Account",
-                              style: TextStyle(color: Colors.amber),
-                            ))
-                      ],
-                    )
-                  ]),
-                ),
-              ),
+            const SizedBox(
+              height: 5,
             ),
-          )
-        ])),
+            ElevatedButton(
+              onPressed: () async {
+                if (_formkey.currentState!.validate()) {
+                  setState(() {
+                    isloading = true;
+                  });
+
+                  final user = await _auth.logInwithEmailandpassword(
+                      _emailcontroller.text, _passwordcontroller.text);
+
+                  if (user != null) {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: ((context) => Dashboard())));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Invalid Credentials")));
+                    setState(() {
+                      isloading = false;
+                    });
+                  }
+                }
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.amber),
+                fixedSize:
+                    MaterialStatePropertyAll(Size(width * 0.6, height * 0.02)),
+              ),
+              child: isloading
+                  ? Container(
+                      height: 10,
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                    )
+                  : const Text(
+                      "LOGIN",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                    ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have account?",
+                  style: TextStyle(color: Colors.grey.shade500),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: ((context) => SignIn())));
+                    },
+                    child: const Text(
+                      "Create Account",
+                      style: TextStyle(color: Colors.amber),
+                    ))
+              ],
+            )
+          ]),
+        ),
       ),
     );
-    ;
   }
 }
--- lib/pages/resetpassword.dart
+++ lib/pages/resetpassword.dart
@@ -8,13 +8,30 @@ class ResetPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Reset Password")),
+      appBar: AppBar(
+        title: Text("Reset Password"),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
       body: Center(
           child: Container(
         child: Column(children: [
           Text("Password reset link will be sent to user inputted email"),
           Container(
-            child: TextField(),
+            height: MediaQuery.of(context).size.height * 0.04,
+            width: MediaQuery.of(context).size.width * 0.7,
+            child: TextField(
+              cursorHeight: 23,
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Theme.of(context).primaryColor),
+                      borderRadius: BorderRadius.circular(20)),
+                  border: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Theme.of(context).primaryColor),
+                      borderRadius: BorderRadius.circular(20))),
+            ),
           )
         ]),
       )),
